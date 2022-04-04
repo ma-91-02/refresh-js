b332c5ceec295c42dcdc9f13a5a83d208d6d9c40
ma-91-02 <ma.91.02@mail.ru>
2022-04-04 09:51:32 +0300

    this is code refresher before node js

--- 003.js
+++ 003.js
@@ -1,9 +1,9 @@
 const person = {
-    name: 'Mohamed',
-    age: 30,
-    greet(){
-        console.log('Hi, I am ' + this.name);
-    },
+  name: "Mohamed",
+  age: 30,
+  greet() {
+    console.log("Hi, I am " + this.name);
+  },
 };
 
 person.greet();
